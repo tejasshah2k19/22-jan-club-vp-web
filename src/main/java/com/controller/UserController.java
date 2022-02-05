054e9355616bf29decc425c3b1ee1bb470b0ad91
Tejas <tejas14all@gmail.com>
2022-02-05 14:40:59 +0530

    model

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,12 +16,12 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/saveuser", method = RequestMethod.POST)
-	public String saveUser(UserBean user) {
+	public String saveUser(UserBean user,Model model) {
 
 		System.out.println(user.getEmail());
 		System.out.println(user.getFirstName());
 		// temp->container -->bean
-
+		model.addAttribute("user",user);//
 		return "Home";
 	}
 
